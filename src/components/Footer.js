b4d85cb3a5020d3198526be4bc9e871b64866c68
AK-deshmukh96 <akawade777@gmail.com>
2024-02-24 18:33:38 +0530

    fix issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,7 @@ const Footer = () => {
         <div className="row">
           <div className="col-1">  
          <h1>We are dedicated to providing the best shopping experience for our customers.</h1>
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/shop">Shop</a></li>
-              <li><a href="/about">About</a></li>
-            </ul>
+           
           </div>
           <div className="col-3">
             <h5>Contact Us</h5>
